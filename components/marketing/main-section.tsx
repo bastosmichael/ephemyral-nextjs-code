cd8f0a2a238ec543b0c0a9674ada0554c4443a63
Michael Bastos <mbastos@zcoregroup.com>
2024-07-24 00:41:41 -0500

    fixing homepage

--- components/marketing/main-section.tsx
+++ components/marketing/main-section.tsx
@@ -8,14 +8,14 @@ export default function MainSection() {
   return (
     <section>
       <h1 className="text-primary text-balance bg-gradient-to-br bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter sm:text-6xl md:text-7xl lg:text-8xl">
-        Ship faster
+        Accelerate Your Development
         <br className="hidden md:block" /> with Ephemyral
       </h1>
 
       <p className="text-primary/80 mb-12 text-lg tracking-tight md:text-xl">
-        Connect to your GitHub, <br className="hidden md:block" />
-        fetch your repositories and issues, <br className="hidden md:block" />
-        and generate LLM-created PRs!
+        Integrate with GitHub, <br className="hidden md:block" />
+        access your repositories and issues, <br className="hidden md:block" />
+        and create ML-driven PRs effortlessly!
       </p>
 
       <Link href="/onboarding">
