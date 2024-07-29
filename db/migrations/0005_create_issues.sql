25a5dab97b3d6d98151da8615a96e3168de68114
Michael Bastos <bastosmichael@gmail.com>
2024-07-29 16:14:06 -0500

    fixing migrations

--- db/migrations/0005_create_issues.sql
+++ db/migrations/0005_create_issues.sql
@@ -5,8 +5,11 @@ CREATE TABLE IF NOT EXISTS "issues" (
 	"name" text NOT NULL,
 	"content" text NOT NULL,
 	"status" text DEFAULT 'ready' NOT NULL,
+	"runner" text,
 	"pr_link" text,
 	"pr_branch" text,
+	"plan_response" text,
+	"code_gen_response" text,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
--- db/migrations/0014_add_prompt_responses.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "issues" ADD COLUMN "plan_response" text;
-ALTER TABLE "issues" ADD COLUMN "code_gen_response" text;
