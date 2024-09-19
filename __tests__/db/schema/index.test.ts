83c7d03f18a9c6fd73996e4cd8563813484f162f
Michael Bastos <bastosmichael@gmail.com>
2024-09-19 15:24:03 -0500

    Update multiple files

---
+++ __tests__/db/schema/index.test.ts
@@ -0,0 +1,17 @@
+import { embeddedFilesTable } from '../../db/schema/embedded-files-schema';
+import { expect } from '@jest/globals';
+
+describe('Embedded Files Schema', () => {
+  it('should have a valid structure', () => {
+    const schema = embeddedFilesTable;
+
+    expect(schema).toHaveProperty('id');
+    expect(schema).toHaveProperty('userId');
+    expect(schema).toHaveProperty('projectId');
+    expect(schema).toHaveProperty('embeddedBranchId');
+    expect(schema).toHaveProperty('githubRepoFullName');
+    expect(schema).toHaveProperty('path');
+    expect(schema).toHaveProperty('tokenCount');
+    expect(schema).toHaveProperty('content');
+  });
+});
