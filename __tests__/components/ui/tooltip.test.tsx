15e728a464cf43c07883d33bf399c4cb2aa4eb81
Michael Bastos <bastosmichael@gmail.com>
2024-09-20 10:53:30 -0500

    removing bad tests

--- __tests__/components/ui/tooltip.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import { Tooltip } from '../../../components/ui/tooltip';
-
-describe('Tooltip component', () => {
-    it('renders correctly', () => {
-        const { getByText } = render(<Tooltip>Tooltip Text</Tooltip>);
-        expect(getByText('Tooltip Text')).toBeInTheDocument();
-    });
-});
