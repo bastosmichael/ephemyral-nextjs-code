443110e5ef92ec386cef972768e2084f371aef00
Michael Bastos <bastosmichael@gmail.com>
2024-09-19 22:04:38 -0500

    removing tests

--- __tests__/actions/github/auth.test.ts
+++
@@ -1,9 +0,0 @@
-import { getAuthenticatedOctokit } from '../../../actions/github/auth';
-    import { expect } from '@jest/globals';
-
-    describe('GitHub Authentication', () => {
-      it('should return an authenticated Octokit instance', async () => {
-        const octokit = await getAuthenticatedOctokit(12345);
-        expect(octokit).toHaveProperty('request');
-      });
-    });
